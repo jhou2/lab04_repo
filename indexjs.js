17ee194556b0118d3977bcec8bc44732a0cd1976
Chris <chrisqin1235@hotmail.com>
2018-11-20 10:01:24 -0800

    index.html

--- indexjs.js
+++ indexjs.js
@@ -1,19 +1,37 @@
-// $('#personalInfo').hide();
-// $('#brandInfo-computer').hide();
-// $('#brandInfo-phone').hide();
+$('#personalInfo').hide();
+$('#brandInfo-computer').hide();
+$('#brandInfo-phone').hide();
 
 
-// $('#submit1').click(function(e){
-//  // $('#deviceInfo').hide();
-//  if ($('#')) {}
-//   $('#brandInfo-computer').toggle();
-//   e.preventDefault();
+$('#submit1').click(function(e){
+ // $('#deviceInfo').hide();
+ if ($('#')) {}
+  $('#brandInfo-computer').toggle();
+  e.preventDefault();
+});
+
+$('#submit2').click(function(e){
+  $('#deviceInfo').hide();
+  $('#brandInfo').hide();
+
+  $('#personalInfo').toggle();
+  e.preventDefault();
+});
+
+// var view = $("#tslshow");
+// var move = "100px";
+// var sliderLimit = "-1000";
+//
+// $("#rightArrow").click(function(){
+//
+//     var currentPosition = parseInt(view.css("left"));
+//     if (currentPosition >= parseInt(sliderLimit)) view.stop(false,true).animate({left:"-="+move},{ duration: 400})
+//
 // });
-
-// $('#submit2').click(function(e){
-//   $('#deviceInfo').hide();
-//   $('#brandInfo').hide();
-
-//   $('#personalInfo').toggle();
-//   e.preventDefault();
+//
+// $("#leftArrow").click(function(){
+//
+//     var currentPosition = parseInt(view.css("left"));
+//     if (currentPosition <= 0) view.stop(false,true).animate({left:"+="+move},{ duration: 400})
+//
 // });
