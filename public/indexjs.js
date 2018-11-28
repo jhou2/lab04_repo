a1ff0073f4b30c18bc39e864ed3178382e612702
Jessica Kim <seung2613@gmail.com>
2018-11-28 00:56:58 -0800

    asd

--- public/indexjs.js
+++ public/indexjs.js
@@ -35,29 +35,29 @@ formSubmit.on('click', function(){
 		// var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
 		var model = snapshot.val();
 		console.log(model);
-		$('#shopping').append($('<p>').text(modelName + " " + model.price)); //???????????????????
+		$('#shopping').append($('<p>').text(modelName + " " + model.price)); //??
 	});
 });
 
+var count = 0;
 var checkout = $('#checkout');
 checkout.on('click', function(){
-
-	function writeUserData(name, address, email, phone, info,
-		 billing_name, billing_phone, billing_address, billing_city,
-		 billing_province, billing_postalcode, billing_email ) {
-		firebase.database().ref('/users/' + userId).set({
-			username: name,
-			address: address,
-			email: email,
-			phone_number: phone,
-			additional_info: info,
-			billing_name:billing_name,
-			billing_phone: billling_email,
-			billing_address: billing_address,
-			billing_city : billing_city,
-			billing_province: billing_province,
-			billing_postalcode: billing_postalcode,
-			billing_email: billing_email
+count ++;
+console.log("hello");
+	function writeUserData() {
+		firebase.database().ref('/users/'+ count).set({
+			username: $('#name').val(),
+			address: $('#address').val(),
+			email: $('#email').val(),
+			phone_number: $('#phone').val(),
+			additional_info: $('#info').val(),
+			billing_name:$('#billing-name').val(),
+			billing_phone: $('#billing-phone').val(),
+			billing_address: $('#billing-address').val(),
+			billing_city : $('#billing-city').val(),
+			billing_province: $('#billing-province').val(),
+			billing_postalcode: $('#billing-postalcode').val(),
+			billing_email: $('#billing-email').val()
 
 		});
 	}
