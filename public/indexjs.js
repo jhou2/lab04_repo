9f6fb8c8de5120d5086632a7df2c27e8ec13c0ea
Jessica Kim <seung2613@gmail.com>
2018-11-29 14:37:31 -0800

    d

--- public/indexjs.js
+++ public/indexjs.js
@@ -2,8 +2,10 @@
 // $('#brandInfo').css('display','none');
 var brandName = "";
 var modelName = "";
+var newKey = "";
 var cart = [];
-let list = ['Apple', 'Dell', 'LG', 'Samsung', 'Sony'];
+var itemsInCart = [];
+let list = ['Apple', 'Dell', 'LG', 'Samsung', 'Hwawei'];
 
 	// Event handler when computer is clicked
 // for (let k = 0; k < 1; k++) {
@@ -36,8 +38,8 @@ formSubmit.on('click', function(){
 	modelName = $('#input').val();
 	firebase.database().ref('/phone/' + brandName + '/' + modelName).once('value').then(function(snapshot) {
 
-
 		var model = snapshot.val();
+		var itemName = modelName + " " + model.price;
 		console.log(model);
 		if (model==null) {
 			alert("We're currently not accepting this model.")
@@ -45,10 +47,14 @@ formSubmit.on('click', function(){
 
 
 
-		var item = $('#shopping').append($('<p>').text(modelName + " " + model.price));
+
+		var item = $('#shopping').append($('<p>').text(itemName));
 		// var item = $('#shopping').append($('<p>').text(modelName + " " + model.price));
 		// var cart = [];
 		// cart.push(item);
+		itemsInCart.push(itemName);
+		console.log(itemsInCart);
+
 
 		var sum = 0;
 		sum = parseInt(model.price.substring(1, model.price.length));
@@ -58,7 +64,7 @@ formSubmit.on('click', function(){
 		for (var i = 0; i < cart.length; i++){
 			total += cart[i];
 			$('#total').html("Total: $"+total);
-			console.log(total);
+			// console.log(total);
 		}
 
 }
@@ -67,10 +73,6 @@ formSubmit.on('click', function(){
 
 
 });
-
-function callback(){
-
-}
 
 var count = 0;
 var checkout = $('#checkout');
@@ -88,9 +90,25 @@ checkout.on('click', function(){
 			billing_city : $('#billing-city').val(),
 			billing_province: $('#billing-province').val(),
 			billing_postalcode: $('#billing-postalcode').val(),
-			billing_email: $('#billing-email').val()
-
+			billing_email: $('#billing-email').val() ,
+			cart: itemsInCart
 		});
+
+
+		// const postKey = firebase.database().ref().child('users').push().key;
+
+
+
+
+
+
+
+			// firebase.database().ref('/phone/' + brandName + '/' + modelName).once('value').then(function(snapshot) {
+			//
+			//
+			//
+			// });
+
 
 	// }
 
